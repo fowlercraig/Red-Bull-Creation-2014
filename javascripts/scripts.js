8a5eb9a243b776c28112ece1d1e4e0095cfb3edb
fowlercraig <fowler@Craigs-iMac.local>
2014-05-01 12:21:00 -0700

    Hooked up Smartajax

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,3 +1,5 @@
+SmartAjax_load('wp-content/themes/redbull-creation-2014/javascripts', function () {
+
 function onepage() {
 
 $("#one-pager").onepage_scroll({
@@ -22,3 +24,24 @@ onepage();
 }
 
 init();
+
+SmartAjax.bind('a', {
+  reload: false,
+  cache: true,
+  containers: [{
+    selector: '#info'
+  }],
+
+  before: function () {
+    $('#ajax-loader').show(); // Show the div
+    SmartAjax.proceed(); // Start loading...
+  },
+
+  done: function () {
+    $('#ajax-loader').hide(); // Hide the div
+    init();
+  }
+
+});
+
+}, true);
